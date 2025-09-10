41bc9b212fd9e81bb24c416ec48262de7ecb249b
Liliya Vildanova <liliya.vild@gmail.com>
2025-09-10 19:23:02 -0400

    Fix blog post query to use fields.slug and add SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -7,38 +7,38 @@
 
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { Helmet } from "react-helmet"
 
 function Seo({ description, title, children }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+          author
         }
       }
-    `
-  )
+    }
+  `)
 
-  const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
+  const siteMetadata = data?.site?.siteMetadata || {}
+  const metaDescription = description || siteMetadata.description
+  const defaultTitle = siteMetadata.title
 
   return (
-    <>
+    <Helmet>
       <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
       <meta name="description" content={metaDescription} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
+      <meta name="twitter:creator" content={siteMetadata.author || ``} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
       {children}
-    </>
+    </Helmet>
   )
 }
 
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,17 +1,19 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import Seo from "../components/seo"
 
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date(formatString: "MMMM D, YYYY")
-      }
-      html
+  markdownRemark(fields: { slug: { eq: $slug } }) {
+    frontmatter {
+      title
+      date(formatString: "MMMM D, YYYY")
     }
+    excerpt
+    html
   }
+}
 `
 
 const BlogPost = ({ data }) => {
@@ -19,6 +21,7 @@ const BlogPost = ({ data }) => {
 
   return (
     <Layout>
+      <Seo title={post.frontmatter.title} description={post.excerpt} />
       <div className="px-6 py-8 max-w-3xl mx-auto">
         <h1 className="text-3xl font-bold text-blue-700 mb-2">{post.frontmatter.title}</h1>
         <p className="text-sm text-gray-500 mb-6">{post.frontmatter.date}</p>
