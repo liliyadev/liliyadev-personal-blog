63583e13d23be80e526af7128546abb25087b82e
Liliya Vildanova <liliya.vild@gmail.com>
2025-09-10 15:37:31 -0400

    Fix blog post slugs and dynamic page creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -58,7 +58,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const value = `/blog${createFilePath({ node, getNode })}`
 
     createNodeField({
       name: `slug`,
@@ -67,3 +67,4 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
