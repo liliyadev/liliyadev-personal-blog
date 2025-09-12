f1e60f6b06bd3039250a0018ff9b0312c82c6050
Liliya Vildanova <liliya.vild@gmail.com>
2025-09-12 10:09:36 -0400

    Replace header with animated Navbar and dark mode toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,25 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     "./content/**/*.{md,mdx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
   darkMode: 'class',
+  theme: {
+    extend: {
+      typography: ({ theme }) => ({
+        invert: {
+          css: {
+            color: theme('colors.white'),
+            a: { color: theme('colors.indigo.300') },
+            strong: { color: theme('colors.white') },
+            h1: { color: theme('colors.white') },
+            h2: { color: theme('colors.white') },
+            h3: { color: theme('colors.white') },
+            code: { color: theme('colors.indigo.300') },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
 }
 
 
